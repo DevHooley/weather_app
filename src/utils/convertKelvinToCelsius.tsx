79f38785d3f1ff112f515d26757270f6b4c74b64
Your Name <92824573+kyleehooley@users.noreply.github.com>
2024-03-11 17:15:24 -0500

    latest

--- src/utils/convertKelvinToCelsius.tsx
+++
@@ -1,5 +0,0 @@
-export function convertKelvinToCelsius(tempInKelvin: number): number {
-  const tempInCelsius = tempInKelvin - 273.15;
-  return Math.floor(tempInCelsius); // round down to the nearest whole number
-}
-// Path: src/utils/cn.ts
